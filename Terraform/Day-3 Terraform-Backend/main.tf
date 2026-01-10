78905863d245c34273a545a8ca267ab1cee0ad7a
Itzaadiiiii <adityapatil14920251@gmail.com>
2026-01-10 20:52:10 +0530

    Add teraform S3 Backend

--- Terraform/Day-3 Terraform-Backend/main.tf
+++ Terraform/Day-3 Terraform-Backend/main.tf
@@ -5,11 +5,18 @@ terraform {
       # version = "~> 5.0"
       version = "6.27.0"
     }
-
   }
-  required_version = ">= 1.0"
+  required_version = ">= 1.14.0"
+
+  backend "s3" {
+    bucket = "terraform-bucketttttt-2026"
+    key    = "/terraform.tfstate"
+    region = "us-east-1"
+    use_locking = true
+  }
 }
 
+  
 provider "aws" {
   region = "us-east-1"
 }
